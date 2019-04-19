680c5ef68f3fe0ebc81a5a8dbb4b26da23479ef1
chengyong <chengyong0807@qq.com>
2019-04-19 14:56:50 +0800

    first modify

---
+++ cn/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+# Create by panzekai
+# Create on 2019/4/19
---
+++ cn/chy/HelloWorld.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+# Create by panzekai
+# Create on 2019/4/19
+
+if __name__ == '__main__':
+    while 1 == 1 :
+        myname = input("你要输入的数据:")
+        if  myname == "q" :
+            break
+        elif myname == "a" :
+            print("--------"+myname)
+        else:
+            print(myname)
